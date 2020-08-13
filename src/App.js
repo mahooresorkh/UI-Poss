2c50ba5518c8b255c95af0cf11b363b5fe3cfd3f
mahooresorkh <mahooresorkh@gmail.com>
2020-08-13 16:49:28 -0700

    some changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,168 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faSortDown } from '@fortawesome/free-solid-svg-icons'
+import { Button, Input } from 'antd';
+import pencil from './images/pencil.png';
+import {buttonStyle, buttonText, buttonImg, deleteButtonStyle, 
+  deleteButtonText, circleButtonImg, circleButtonStyle, cardDiv,
+  cardHeader, cardBody, cardHeaderRegNo, cardHeaderFlex, dropDownButtonText,
+  dropDownButton, sortDownIcon, inputBox, inputLabel, inputSelf} 
+  from './supplementaryStyles';
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      togglerWindowIsVisible:true,
+      togglerWindow:{}
+      
+    }
+  }
+  togglerWindowHeight = null;
+  componentDidMount(){
+    this.togglerWindowHeight = document.getElementById('toggler').offsetHeight;
+    this.setState({togglerWindow:{
+      transition: 'height 2s ease-out',
+      overflowY:'hidden',
+      height:`${this.togglerWindowHeight}px`
+    }})
+  }  
+  showDetails = () =>{
+    if(this.state.togglerWindowIsVisible){
+      this.setState({togglerWindow:{
+        transition: 'height 2s ease-out',
+        overflowY:'hidden',
+        height:'0'}
+      })
+        
+    }
+    else{
+      this.setState({togglerWindow:{
+        transition: 'height 2s ease-out',
+        overflowY:'hidden',
+        height: this.togglerWindowHeight
+      }
+      })
+    }
 
-function App() {
+    this.setState({togglerWindowIsVisible:!this.state.togglerWindowIsVisible})
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <Button type="primary" style={buttonStyle}>
+          <img src={pencil} style={buttonImg} />
+          <span style={buttonText}>امضا</span>
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Button type="primary" style={buttonStyle}>
+          <img src={pencil} style={buttonImg} />
+          <span style={buttonText}>کپی</span>
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Button type="primary" style={buttonStyle}>
+          <img src={pencil} style={buttonImg} />
+          <span style={buttonText}>بایگانی</span>
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Button type="primary" style={buttonStyle}>
+          <img src={pencil} style={buttonImg} />
+          <span style={buttonText}>تایید</span>
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Button type="primary" style={buttonStyle}>
+          <img src={pencil} style={buttonImg} />
+          <span style={buttonText}>ویرایش</span>
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Button type="primary" style={deleteButtonStyle}>
+          <span style={deleteButtonText}>حذف</span>
+        </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        {/* 
+        npm i --save @fortawesome/fontawesome-svg-core
+        npm install --save @fortawesome/free-solid-svg-icons
+        npm install --save @fortawesome/react-fontawesome */}
+        <Button type="primary" style={circleButtonStyle}>
+          <FontAwesomeIcon icon={faHome} size="lg"/>
+        </Button>
+      </div>
+      <br/>
+      <div>
+        <div style={cardDiv}>
+          <div style={cardHeader}>
+            <div style={cardHeaderFlex}> 
+              <div>1399/05/23</div>
+              <div>در سدهٔ نوزدهم م، رویارویی با استعمار اروپای مدرن</div>
+            </div>
+            <div style={cardHeaderRegNo}>654132135432</div> 
+          </div>
+          
+          <div style={cardBody}>
+          سرزمین ایران، میزبان تمدن‌های کهنی چون ایلام، جیرفت و زاینده‌رود بوده‌است. نخستین‌بار در سدهٔ هفتم پیش از میلاد، در دوران پادشاهی ماد بود که بخش‌های قابل توجهی از فلات ایران یکپارچه شد. در سدهٔ ششم پ. م، شاهنشاهی هخامنشی توسط کوروش بزرگ بنیان نهاده شد تا ایران یکی از بزرگ‌ترین امپراتوری‌های تاریخ را تشکیل دهد. در سدهٔ چهارم پ. م، اسکندر مقدونی این امپراتوری را پایان داد و ایران به بخشی از ممالک هلنیستی تبدیل شد. پدیداری شاهنشاهی اشکانی در سدهٔ سوم پ. م، بار دیگر این کشور را تحت فرمان یک شاهنشاهی ایرانی قرار داد. در سدهٔ سوم م، شاهنشاهی ساسانی، یک امپراتوری گستردهٔ دیگر، در ایران به قدرت رسید و برای چهار سده بر سرزمینی پهناور حکومت کرد و مزدیسنا به دین غالب آن، تبدیل شد. ایران در این دوران نیز درگیر جنگ‌های مستمر و فرساینده با روم بود که به تضعیف کشور انجامید. در میانه‌های سدهٔ هفتم م، مسلمانان، امپراتوری ساسانی را سرنگون کردند و اسلام را به جای دین‌های ایرانی، رواج دادند. از دوران خلافت اسلامی تا سدهٔ سیزدهم، فعالیت‌های ادبی، علمی و هنری ایرانی نه تنها پایان نیافت، بلکه بار دیگر به شکوفایی رسید و ایرانیان مشارکتی اثرگذار در شکل‌گیری دوران طلایی اسلام داشتند. از سدهٔ نهم م، میان‌دورهٔ ایرانی آغاز شد و نخستین حکومت‌های ایرانی‌تبار پس از اسلام، پدیدار شدند. در سدهٔ دهم م، اقوام ترک به این کشور آمدند و حکومت‌هایی را تشکیل دادند که بر بخش بزرگی از ایران، حکومت می‌کردند. از سدهٔ ۱۳ م، حملهٔ مغول به ایران روی داد که به تشکیل ایلخانان انجامید و پس از آن، امپراتوری تیموری پدیدار شد.
+          با پدیداری صفویان، رونق اقتصادی و پایداری مرزها نمود بیشتری یافت و ایران پس از حدود ۹ سده، تحت یک حکومت مستقل بومی، متحد شد و مذهب آن به شیعه تغییر یافت. پس از سرنگونی صفویان، دودمان‌های افشاریان و زندیان به ترتیب بر ایران، فرمان راندند. در دوران قاجاریان، جنگ‌هایی با روسیه انجام شد که سرزمین‌های قابل توجهی را از این کشور جدا کرد؛ در سدهٔ نوزدهم م، رویارویی با استعمار اروپای مدرن، حاکمیت ملی را تضعیف کرد. در دههٔ ۱۲۸۰، جنبش مشروطهٔ ایران قدرت گرفت و قانون اساسی مشروطه را بر این کشور، حاکم کرد. در سال ۱۳۰۴، شاهنشاهی پهلوی توسط رضاشاه بنیان نهاده شد، این دوران با اصلاحات گسترده و ایجاد زیرساخت نوین برای ایران، همراه شد. دوران محمدرضا پهلوی اصلاحات ادامه یافت و ایران رشد اقتصادی سریعی را شاهد بود؛ در این دوران، صنعت نفت ایران، ملی شد.
+          </div>
+        </div>
+
+      </div>
+
+      <br />
+      <br />
+
+      <div style={{backgroundColor:'grey', padding:'30px'}}>
+        <Button style={dropDownButton} type="primary" onClick={this.showDetails}>
+          <FontAwesomeIcon icon={faSortDown} size="lg" style={sortDownIcon}/>
+          <span style={dropDownButtonText}>جزئیات نامه</span> 
+        </Button>
+      </div>
+      <br />
+      <div id='toggler' style={this.state.togglerWindow}>
+        <div style={cardDiv}>
+          <div style={cardHeader}>
+            <div style={cardHeaderFlex}> 
+              <div>1399/05/23</div>
+              <div>در سدهٔ نوزدهم م، رویارویی با استعمار اروپای مدرن</div>
+            </div>
+            <div style={cardHeaderRegNo}>654132135432</div> 
+          </div>
+          
+          <div style={cardBody}>
+            <div style={inputBox}>
+              <Input style={inputSelf} />
+              <div style={inputLabel}>
+                شماره ثبت
+              </div>
+            </div>
+            <br/>
+            <div style={inputBox}>
+              <Input style={inputSelf} />
+              <div style={inputLabel}>
+                شماره ثبت
+              </div>
+            </div>
+            <br/>
+            <div style={inputBox}>
+              <Input style={inputSelf} />
+              <div style={inputLabel}>
+                شماره ثبت
+              </div>
+            </div>
+            <br/>
+            <div style={inputBox}>
+              <Input style={inputSelf} />
+              <div style={inputLabel}>
+                شماره ثبت
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span>asl;kdjf;lsakjdf;lkasjdfl;kjsadfl;kjsdf;lkjsaf;lkjasdfl;kjsadfl;kjasdf;lkj</span>
     </div>
   );
+      }
 }
 
 export default App;
---
+++ src/supplementaryStyles.js
@@ -0,0 +1,134 @@
+import pencil from  './images/pencil.png';
+export const buttonStyle = {
+    backgroundColor:'rgb(230,230,230)', 
+    borderRadius:'10px',
+    padding: '0px 2px',
+    height:'35px',
+    border:'0',
+    boxShadow:'0px 1px 2px rgba(100,100,100,0.5)'
+}
+
+export const buttonText = {
+    lineHeight:'0',
+    margin:'5px',
+    color:'rgb(100,100,100)',
+    fontFamily:'BYekan'
+}
+
+export const buttonImg = {
+    width:'30px',
+    height:'30px'
+}
+
+export const deleteButtonStyle = {
+    backgroundColor:'rgb(8,86,0)', 
+    borderRadius:'10px',
+    padding: '0px 2px',
+    height:'35px',
+    border:'0',
+    boxShadow:'0px 0px 3px rgba(100,100,100,0.9)'
+}
+
+export const deleteButtonText= {
+    lineHeight:'0',
+    margin:'10px',
+    color:'rgb(220,220,220)',
+    fontFamily:'BYekan'
+}
+
+export const circleButtonStyle={
+    backgroundColor:'rgb(100,100,100)', 
+    borderRadius:'80px',
+    padding: '0px 2px',
+    height:'32px',
+    width:'32px',
+    border:'3px solid white',
+    boxShadow:'0px 1px 2px rgba(100,100,100,0.5)',
+
+}
+
+export const cardDiv =  {
+    maxWidth:'1000px',
+    backgroundColor:'white',
+    borderRadius:'30px',
+    margin:'0px auto',
+    boxShadow:'0px 1px 2px rgba(100,100,100,0.5)'
+}
+
+export const cardHeader = {
+    position:'relative',
+    backgroundColor:'rgb(100,100,100)',
+    height:'40px',
+    borderRadius:'30px 30px 0px 0px',
+    fontFamily:'BYekan',
+    color:'white',
+}
+
+export const cardHeaderFlex= {
+    display:'flex',
+    justifyContent:'space-between',
+    alignItems: 'center',
+    height:'100%',
+    padding:'0px 15px'
+}
+
+export const cardBody = {
+    padding:'15px',
+    maxHeight:'300px',
+    overflowY:'auto',
+    textAlign:'right',
+    fontFamily:'BYekan',
+}
+
+
+export const cardHeaderRegNo = {
+    position:'absolute',
+    textAlign:'center',
+    top:'9px',
+    height:'100%',
+    width:'100%'
+}
+
+export const dropDownButtonText = {
+    fontFamily:'BYekan',
+    marginLeft:'10px',
+    marginBottom:'4px',
+    color:'rgb(100,100,100)',
+    
+}
+
+export const dropDownButton = {
+    height:'30px',
+    padding:'0px 17px',
+    borderRadius:'8px',
+    backgroundColor:'rgb(245,245,245)',
+    border:'0',
+    boxShadow:'0px 3px 2px rgb(0,0,0,0.4)'
+    
+}
+
+export const sortDownIcon = {
+    color:'rgb(233,180,88)'
+}
+
+export const inputBox ={
+    display:'flex'
+}
+
+export const inputLabel ={
+    borderRadius:'0px 3px 3px 0px',
+    width:'90px',
+    lineHeight:'2.1',
+    backgroundColor:'rgb(147,186,191)',
+    textAlign:'center',
+    boxShadow:'-1px 0px 5px rgba(0,0,0,0.3)',
+    position:'relative',
+    zIndex:'100',
+    color:'white'
+}
+export const inputSelf ={
+    backgroundColor:'rgb(216,216,216)',
+    borderRadius:'3px 0px 0px 3px',
+
+}
+
